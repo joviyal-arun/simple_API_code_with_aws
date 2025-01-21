f23306b7a3152902a39c3ae2ed6930539d66eff5
joviyal-arun <joviyalarun07@gmail.com>
2025-01-21 11:25:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Route to handle button click
+@app.route("/button-click")
+def button_click():
+    return "<h1>Button clicked! Here is your static content.</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
